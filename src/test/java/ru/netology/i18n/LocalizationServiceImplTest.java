5e7b1a8dbf33825489a915fa16b321d2b06ebcaa
sandal <ru-sandal@yandex.ru>
2022-03-29 22:02:09 +0300

    Added readme.md

--- src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
+++ src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
@@ -1,14 +1,14 @@
 package ru.netology.i18n;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import ru.netology.entity.Country;
 
+
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LocalizationServiceImplTest {
 
