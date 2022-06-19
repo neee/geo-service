467ad108b172ea944e38519042631f33cabad24b
Mikhail Machulski <mik.machulski@gmail.com>
2022-06-19 21:01:26 +0300

    New tests and corrections added

--- src/test/java/geoServiceTest.java
+++ src/test/java/geoServiceTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import ru.netology.entity.Country;
@@ -17,46 +18,19 @@ import java.util.Map;
 public class geoServiceTest {
 
     @Test
-    public void testCorrectRussianText() {
-        GeoService geoService = Mockito.mock(GeoService.class);
-        Mockito.when(geoService.byIp("172.2.32.11"))
-                .thenReturn(new Location("Moscow", Country.RUSSIA, null, 0));
+    @DisplayName("Correct localhost IP test")
+    public void testCorrectIpLocalHost() {
+        GeoServiceImpl geoService = new GeoServiceImpl();
+        Location expected = new Location(null, null, null, 0);
+        final String LOCALHOST = "127.0.0.1";
 
-        LocalizationService localizationService = Mockito.mock(LocalizationService.class);
-        Mockito.when(localizationService.locale(Country.RUSSIA))
-                .thenReturn("Добро пожаловать");
-
-        MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
-        Map<String, String> headers = new HashMap<String, String>();
-        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.2.32.11");
-
-        final String result = messageSender.send(headers);
-        final String expected = "Добро пожаловать";
+        final Location result = geoService.byIp(LOCALHOST);
 
         Assertions.assertEquals(result, expected);
     }
 
     @Test
-    public void testCorrectEnglishText() {
-        GeoService geoService = Mockito.mock(GeoService.class);
-        Mockito.when(geoService.byIp("96.45.183.149"))
-                .thenReturn(new Location("New York", Country.USA, null,  0));
-
-        LocalizationService localizationService = Mockito.mock(LocalizationService.class);
-        Mockito.when(localizationService.locale(Country.USA))
-                .thenReturn("Welcome");
-
-        MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
-        Map<String, String> headers = new HashMap<String, String>();
-        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "96.45.183.149");
-
-        final String result = messageSender.send(headers);
-        final String expected = "Welcome";
-
-        Assertions.assertEquals(result, expected);
-    }
-
-    @Test
+    @DisplayName("Correct Moscow IP test")
     public void testCorrectIpLocationMoscow() {
         GeoServiceImpl geoService = new GeoServiceImpl();
         Location expected = new Location("Moscow", Country.RUSSIA, "Lenina", 15);
@@ -68,23 +42,37 @@ public class geoServiceTest {
     }
 
     @Test
-    public void testLocalizationRussia() {
-        LocalizationServiceImpl localizationService = new LocalizationServiceImpl();
-        final String expected = "Добро пожаловать";
-        final Country country = Country.RUSSIA;
+    @DisplayName("Correct New-York IP test")
+    public void testCorrectIpLocationNewYork() {
+        GeoServiceImpl geoService = new GeoServiceImpl();
+        Location expected = new Location("New York", Country.USA, " 10th Avenue", 32);
+        final String NEW_YORK_IP = "96.44.183.149";
 
-        final String result = localizationService.locale(country);
+        final Location result = geoService.byIp(NEW_YORK_IP);
 
         Assertions.assertEquals(result, expected);
     }
 
     @Test
-    public void testLocalizationWorldwide() {
-        LocalizationServiceImpl localizationService = new LocalizationServiceImpl();
-        final String expected = "Welcome";
-        final Country country = Country.USA;
+    @DisplayName("Correct Russian IP test")
+    public void testCorrectIpLocationRussia() {
+        GeoServiceImpl geoService = new GeoServiceImpl();
+        Location expected = new Location("Moscow", Country.RUSSIA, null, 0);
+        final String RUSSIAN_IP = "172.0.0.0";
 
-        final String result = localizationService.locale(country);
+        final Location result = geoService.byIp(RUSSIAN_IP);
+
+        Assertions.assertEquals(result, expected);
+    }
+
+    @Test
+    @DisplayName("Correct American IP test")
+    public void testCorrectIpLocationUSA() {
+        GeoServiceImpl geoService = new GeoServiceImpl();
+        Location expected = new Location("New York", Country.USA, null,  0);
+        final String USA_IP = "96.0.0.0";
+
+        final Location result = geoService.byIp(USA_IP);
 
         Assertions.assertEquals(result, expected);
     }
